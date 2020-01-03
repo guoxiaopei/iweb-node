eaa459d6346044ad27228bf44d9b032e88822cd4
Guo.XP <guoxiaopei@tom.com>
2020-01-03 18:19:44 +0800

    success

--- route/favorite.js
+++ route/favorite.js
@@ -21,9 +21,28 @@ router.post("/add", (req, res) => {
     if(err) throw err;
     console.log(result);
     if(result.affectedRows > 0) {
-      res.json({code: 200, msg: "success"})
+      res.json({code: 200, msg: "success", data: {fid: result.insertId}})
     } else {
-      res.json({code: 300, msg: "failed"})
+      res.json({code: 301, msg: "failed"})
+    }
+  })
+})
+//收藏查询
+router.post("/list", (req, res) => {
+  let obj = req.body;
+  if(!obj.uid) {
+    res.json({code: 301, msg: "uid is required"})
+    return;
+  }
+
+  let sql = "SELECT course.cid,course.title,course.pic,course.price,favorite.fid,favorite.fTime FROM favorite,course WHERE course.cid = favorite.courseId AND favorite.userId = ? ORDER BY favorite.fTime DESC";
+  pool.query(sql, [obj.uid], (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    if(result.length > 0) {
+      res.json({code: 200, msg: "success", data: result})
+    } else {
+      res.json({code: 201, msg: "no favorite"})
     }
   })
 })
