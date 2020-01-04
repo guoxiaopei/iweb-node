3464bb6ec6651ce1c698d6fea4c7e08d5439afd4
Guo.XP <guoxiaopei@tom.com>
2020-01-04 18:08:40 +0800

    跨域

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ app.listen(5050);
 console.log("Server is listening 5050, runing......");
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.get("/hello", (req, res) => {
-  res.send("hello world!");
+
+// 声明跨域
+app.all("*",function(req,res,next){
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin","*");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers","content-type");
+  //跨域允许的请求方式 
+  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+      res.send(200);  //让options尝试请求快速结束
+  else
+      next();
 });
 
 const indexRouter = require("./route/index");
